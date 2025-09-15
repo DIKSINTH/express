6cd73eb9c8c08215a01faa4a4760704ec987a676
DIKSINTH <diksinth1@gmail.com>
2025-09-15 17:08:01 +0530

    new

---
+++ form/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/input", (req, res, next) => {
+  res.send(`
+    <body>
+      <h1>Add Name</h1>
+      <form action="/output" method="POST">
+        <input type="text" name="username" />
+        <input type="submit" />
+      </form>
+    </body>
+  `);
+});
+app.use("/output", (req, res, next) => {
+  console.log("Form Data :", req.body);
+  res.send("<h1>Form Submitted Successfully</h1>");
+});
+
+app.listen(3000);
